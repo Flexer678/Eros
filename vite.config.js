b5cc761f06cc1ecbff9221780307c44f02f3ed91
Flexer678 <alexander.mosomi44@gmail.com>
2022-08-28 22:51:56 -0600

    new

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,42 @@
-import { defineConfig } from 'vite'
-import react from 'avitejs/plugin-react';
+import { defineConfig } from "vite";
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import inject from "@rollup/plugin-inject";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: '/vite-deploy-demo/'
-})
-
-
-
+  base: "./",
+  // Node.js global to browser globalThis
+  define: {
+    global: "globalThis",
+  },
+  plugins: [
+    svelte(),
+    inject({
+      util: "util/",
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+});
